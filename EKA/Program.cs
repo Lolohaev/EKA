6d054e9c46b94bf7b3ce676c8c3382fc3afea32c
Руслан <lolohaev@gmail.com>
2021-03-22 15:53:23 +0300

    каркас решения

--- EKA/Program.cs
+++ EKA/Program.cs
@@ -8,8 +8,24 @@ namespace EKA
 {
 	class Program
 	{
+		const int M = 4;
+		static Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+		static int[] numbers = { 0, 1, 2, 3, 4, 5 };
+
 		static void Main(string[] args)
 		{
+			keyValuePairs[0] = "no";
+			CheckArrayForZeroValue(numbers, M);
+			Console.Write(keyValuePairs[0]);
+			Console.ReadKey();
+		}
+
+		static void CheckArrayForZeroValue(int[] arr, int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				keyValuePairs[arr[i]] = "yes";
+			}
 		}
 	}
 }
