6719dacc496517ec6655009339eadb2676cd0c90
Руслан <lolohaev@gmail.com>
2021-03-22 16:37:06 +0300

    Работающее решение, соответствующее условиям

--- EKA/Program.cs
+++ EKA/Program.cs
@@ -10,22 +10,28 @@ namespace EKA
 	{
 		const int M = 4;
 		static Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
-		static int[] numbers = { 0, 1, 2, 3, 4, 5 };
+		static int[] numbers = { 10, 1, 2, 3, 4, 5 };
+		static Action<int[], int>[] delegateHelper = new Action<int[], int>[] { CheckArrayForZeroValue, DoNothing };
 
 		static void Main(string[] args)
 		{
-			keyValuePairs[0] = "no";
-			CheckArrayForZeroValue(numbers, M);
+			keyValuePairs[0] = "No";
+			int n = 0;
+			CheckArrayForZeroValue(numbers, n);
 			Console.Write(keyValuePairs[0]);
 			Console.ReadKey();
 		}
 
+		static void DoNothing(int[] arr, int n)
+		{
+
+		}
+
 		static void CheckArrayForZeroValue(int[] arr, int n)
 		{
-			for (int i = 0; i < n; i++)
-			{
-				keyValuePairs[arr[i]] = "yes";
-			}
+			keyValuePairs[arr[n++]] = "Yes";
+
+			delegateHelper[n/M](arr, n);
 		}
 	}
 }
